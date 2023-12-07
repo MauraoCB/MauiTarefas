03e6036379a0962dc5de6d52a4ba8bcbad6ec145
jzmsm <jzmsm@DESKTOP-N89BK3J>
2023-12-07 14:31:08 -0300

    Add project files.

---
+++ ViewModels/DetalhesPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MauiTarefas.Pages;
+using System.Collections.ObjectModel;
+
+
+namespace MauiTarefas.ViewModels
+{
+    public partial class DetalhesPageViewModel: ObservableObject
+    {
+        [ObservableProperty]
+        string text;
+
+        [RelayCommand]
+        async Task GoBack()
+        {
+            await Shell.Current.GoToAsync("..");
+        }
+
+        [RelayCommand]
+        async Task GoContato()
+        {
+            await Shell.Current.GoToAsync(nameof(ContatosPage));
+        }
+    }
+}
